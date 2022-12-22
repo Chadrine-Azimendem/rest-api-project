d45fe32ddae943c6256d5e781629e6b123b6e74c
Chadrine-Azimendem <chadrine.azimendem@gmail.com>
2022-12-22 12:21:53 +0000

    some comments

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,12 @@
+// connect to the database cluster
+
+// require the dotenv library
 require("dotenv").config();
 
+// require the mongoose library
 const mongoose = require("mongoose");
 
+// connect to mongodb database through the environment variable MONGO_URI
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
@@ -11,4 +16,5 @@ const dbConnection = async () => {
   }
 };
 
+// call connection function
 dbConnection();
