950a28fd6a1f14077632dd350134207a9ac612c8
Chadrine-Azimendem <chadrine.azimendem@gmail.com>
2022-12-25 18:10:34 +0000

    password security done

--- src/users/userRoutes.js
+++ src/users/userRoutes.js
@@ -7,10 +7,12 @@ const {
   updateUser,
 } = require("./userControllers");
 
+const { hashPassword } = require("../middleware/index");
+
 const password = require("../middleware/password");
 
 const userRouter = Router();
-userRouter.post("/createAUser", password, createUser);
+userRouter.post("/createAUser", password, hashPassword, createUser);
 userRouter.get("/readData", readUsers);
 userRouter.delete("/deleteData", deleteUser);
 userRouter.put("/updateUser", updateUser);
