58e66241609ce33a08b383731a8b04b476e9d989
Chadrine-Azimendem <chadrine.azimendem@gmail.com>
2022-12-31 11:33:56 +0000

    cors required in server.js

--- src/server.js
+++ src/server.js
@@ -1,29 +1,24 @@
 require("./db/connection");
-// require express library
+
 const express = require("express");
 const userRouter = require("./users/userRoutes");
-// import cookie-parser library
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-// import errorHandler module
 const errorHandler = require("./middleware/errorHandlers");
-// call express Application
-const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Hi from Node js");
-// });
+const app = express();
 
 // dfine a port
 const port = process.env.PORT || 5001;
 
 // tell the server to use json format data only
 app.use(express.json());
-// tell the server to use the routes
+
 app.use(userRouter);
-// tell the server to use the cookieParser
 app.use(cookieParser);
-// tell the server to use the errorHandler module
+app.use(cors());
 app.use(errorHandler);
+
 // tell the server to listen to predefined port
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
