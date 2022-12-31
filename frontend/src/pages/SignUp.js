0186dbbca618df18d021d69353f8d7518e4bb818
Chadrine-Azimendem <chadrine.azimendem@gmail.com>
2022-12-31 11:13:52 +0000

    sign in and sign up forms done

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -4,6 +4,21 @@ const SignUp = () => {
   return (
     <div>
       <h1>Sign Up form</h1>
+      <form>
+        <label>
+          Your username:
+          <input type="text" />
+        </label>
+        <label>
+          Your email:
+          <input type="text" />
+        </label>
+        <label>
+          create password:
+          <input type="text" />
+        </label>
+        <button type="submit">Signup</button>
+      </form>
     </div>
   );
 };
